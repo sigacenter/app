a853b5bfb231742c92b047e9fea8b28ebf1bfcdc
sigacenter <sigacentersport@gmail.com>
2026-09-22 10:56:32 +0000

    14

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'siga-shell-v3';
+const CACHE_NAME = 'siga-shell-v4';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
